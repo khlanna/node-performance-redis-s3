b90bb54b36535e503baf55cc1396a6b579eba2c3
Anna Khloyan <anna.khloyan@imid.am>
2026-06-08 15:18:10 +0400

    testing redis

--- config/dev.js
+++ config/dev.js
@@ -3,6 +3,6 @@ module.exports = {
     "70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com",
   googleClientSecret: "8mkniDQOqacXtlRD3gA4n2az",
   mongoURI:
-    "mongodb+srv://readonly:0Wc2It8kdXQ8INbh@cluster0.ffbere1.mongodb.net/advnode?retryWrites=true&w=majority",
+    "mongodb+srv://dbUser:ysbysmyss!1@cluster0.umihlqy.mongodb.net/?appName=Cluster0",
   cookieKey: "123123123",
 };
